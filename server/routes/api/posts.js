5e1286045e504b328495efad403024bea36dc867
jlaird <Jeremy.Laird@cygnus.com>
2020-01-01 08:55:58 -0600

    Start of API

---
+++ server/routes/api/posts.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongodb = require('mongodb');
+require('dotenv').config()
+
+const router = express.Router();
+
+// Get Posts
+router.get('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArry());
+});
+
+// Add Post
+router.post('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date()
+  });
+  res.status(201).send();
+});
+
+const loadPostsCollection = async () => {
+  consts = client = await mongodb.MongoClient.connect(
+    `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@mongo:27017/admin`,
+    {useNewUrlParser: true}
+  );
+  return client.db('arduino').collection('posts');
+}
+
+module.exports = router;
