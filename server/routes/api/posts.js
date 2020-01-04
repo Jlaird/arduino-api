c78b3d39178b6bde85f617ee5928ee40517a2e39
jlaird <Jeremy.Laird@cygnus.com>
2020-01-04 13:17:39 -0600

    Start of Vue frontend

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -7,7 +7,7 @@ const router = express.Router();
 // Get Posts
 router.get('/', async (req, res) => {
   const posts = await loadPostsCollection();
-  res.send(await posts.find({}).toArry());
+  res.send(await posts.find({}).toArray());
 });
 
 // Add Post
