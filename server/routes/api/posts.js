f61e18de3a2ea80113e4454145ecd5e9eb5de15b
jlaird <Jeremy.Laird@cygnus.com>
2020-01-04 18:43:41 -0600

    Added delete to API

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -21,6 +21,13 @@ router.post('/', async (req, res) => {
   res.status(201).send();
 });
 
+// Delete
+router.delete('/:id', async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+  res.status(200).send();
+});
+
 const loadPostsCollection = async () => {
   consts = client = await mongodb.MongoClient.connect(
     `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@mongo:27017/admin`,
